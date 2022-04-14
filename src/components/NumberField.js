7abbbddae35e672eafa0f3c6a0c01b858e8617a2
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-14 10:28:08 -0400

    added the plan to the file NumberField and an export statement

---
+++ src/components/NumberField.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class NumberField extends React.Component {
+    render () {
+        return <input type="number"></input>
+    }
+}
+export default NumberField;
