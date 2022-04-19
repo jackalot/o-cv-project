482e6522e360fbc0478af55de672d5b2c5d8502f
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-19 09:22:14 -0400

    imported react, created a class component, rendered the return div, added a export statement for TextField

---
+++ src/components/TextField.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class TextField extends React.Component {
+    render() {
+        return <div>
+            
+        </div>
+    }
+}
+export default TextField;
