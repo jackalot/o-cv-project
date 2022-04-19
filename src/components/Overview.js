6b4561b446de45b46e2f61dc28068de906117c32
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-19 09:43:14 -0400

    removed the duplicate elements in the OverView component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -16,10 +16,7 @@ class GeneralInformation extends React.Component {
 class OverView extends React.Component {
     render () {
       return <div>
-        <label htmlFor="phoneNumber">Please enter a phone number</label>
-        <input className="phone-number" type="tel" placeholder="+1 123-456-789" pattern="[0-9]{3}-[0-9]{3}-{0-9}{4}"></input>
         <DateField fieldName="dateOfStudy" className="date-of-study" labelText="When did you study here?"></DateField>
-        <TextField fieldName="name" className="our-name" labelText="What is your name?"></TextField>
       </div>
     }
 }
