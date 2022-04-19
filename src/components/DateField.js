7d809d95baa23e2631d6ff8f658eb65804a5e205
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-19 07:52:19 -0400

    added a label text props to the label so we can add text to the label

--- src/components/DateField.js
+++ src/components/DateField.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class DateField extends React.Component {
     render() {
         return <div>
-            <label htmlFor={this.props.fieldName}></label>
+            <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
+            
         </div>
     }
 }
