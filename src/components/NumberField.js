cd6df6d82d653c63d0655764f693cee5160c073d
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-19 08:07:17 -0400

    NumberField now accepts labelText as a prop similar to DateField

--- src/components/NumberField.js
+++ src/components/NumberField.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class NumberField extends React.Component {
     render () {
         return <div>
-            <label htmlFor={this.props.fieldName}></label>
+            <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
         <input className={this.props.className} id={this.props.fieldName} type="number"></input>
         </div>
     }
