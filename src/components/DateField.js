c537f11666efc9a6d76e277d7f2056439f0405f4
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-19 07:51:09 -0400

    DateField now rteturns an empty label

--- src/components/DateField.js
+++ src/components/DateField.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class DateField extends React.Component {
     render() {
         return <div>
-
+            <label htmlFor={this.props.fieldName}></label>
         </div>
     }
 }
