3a3c047225dfd39094b8a9b28e5820391b16de4c
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-19 09:23:55 -0400

    created a input text with a className and a id

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -4,6 +4,7 @@ class TextField extends React.Component {
     render() {
         return <div>
             <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
+            <input className={this.props.className} id={this.props.fieldName} type='text'></input>
         </div>
     }
 }
