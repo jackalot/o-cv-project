03721d7e8bdf787ef886e6d768ff071ae4a2cd6b
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-19 08:55:29 -0400

    added a placeholder attribute and added a US standard for phoneNumbers as a pattern for the phoneNumber input

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -5,7 +5,7 @@ class OverView extends React.Component {
     render () {
       return <div>
         <label htmlFor="phoneNumber">Please enter a phone number</label>
-        <input className="phone-number" type="tel"></input>
+        <input className="phone-number" type="tel" placeholder="+1 123-456-789" pattern="[0-9]{3}-[0-9]{3}-{0-9}{4}"></input>
         <DateField fieldName="dateOfStudy" className="date-of-study" labelText="When did you study here?"></DateField>
       </div>
     }
