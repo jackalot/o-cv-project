fb55625fb6faa414d30469a6129dc714a2e843f5
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-20 10:04:01 -0400

    created a function named handleInput and its changed onCHange in the TextField component

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 
 class TextField extends React.Component {
+    handleInput = (input) => {
+
+    }
     render() {
         return <div>
             <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
-            <input className={this.props.className} id={this.props.fieldName} type='text'></input>
+            <input onChange={this.handleInput} className={this.props.className} id={this.props.fieldName} type='text'></input>
         </div>
     }
 }
