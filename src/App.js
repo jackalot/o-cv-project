9cda29972b30be3001cffe93fa8a21593e57534f
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-20 08:39:08 -0400

    added a onSubmitTask function to the App component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import './App.css';
 import OverView from './components/Overview';
 class App extends React.Component {
+  onSubmitTask = () => {
 
+  }
   render() {
     return (
       <div className="App">
