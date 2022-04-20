76454c149cdad8ffc881c5567b435e988ba48022
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-20 09:29:16 -0400

    added a constructor and super props with a state thats set with some default variables

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import React from 'react';
 import './App.css';
 import OverView from './components/Overview';
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      generalInformation: {
+        name: "John",
+        email: "john@emailaddress.com",
+        phoneNumber: "+1 123-456-789"
+      },
+    }
+  }
   onSubmitTask = () => {
     this.setState({
      generalInformation: {
