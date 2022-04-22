c019a3435ad192579052ba56cfe42321525e1cc9
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-22 10:36:47 -0400

    removed the state from TextField

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 
 class TextField extends React.Component {
-    state = {
-        input: "Sample",
-    }
     render() {
         return <div>
             <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
