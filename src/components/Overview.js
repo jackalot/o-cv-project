e1378ed2d7b5f6aa7f85613b52641934e0df0f8b
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-22 09:31:17 -0400

    removed state from OverVIew

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -63,19 +63,6 @@ class GeneralInformation extends React.Component {
   }
 }
 class OverView extends React.Component {
-  constructor (props)
-  {
-    super(props)
-    this.state  = { 
-      practicalExperience: {
-      companyName: "sample company",
-      positionTitle: 'sample position',
-      mainTasks: 'sample main tasks',
-      startDate: '1-31-2000',
-      endDate: '1-31-2000'
-      },
-    }
-  }
     render () {
       return <div>
         <GeneralInformation></GeneralInformation>
