0bf5d9e25170d7e27fa7abe2c248a03006684a52
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-25 08:38:08 -0400

    gave TextField its own state and an else statemen to the render, so now it will handle its own state if it can't get an input

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 
 class TextField extends React.Component {
+    state = {
+        text: ''
+    };
+    handleInput = ( event ) => {
+        this.setState({ text: event.target.value });
+    };
     render() {
-        return <div>
+        if(this.props.handler)
+        {
+            return <div>
+                <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
+                <input onChange={this.props.handler} className={this.props.className} id={this.props.fieldName} type='text'></input>
+            </div>
+        }
+        else
+        {
+            return <div>
             <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
-            <input onChange={this.props.handler} className={this.props.className} id={this.props.fieldName} type='text'></input>
+            <input onChange={this.handleInput} className={this.props.className} id={this.props.fieldName} type='text'></input>
         </div>
+        }
     }
 }
 export default TextField;
