bbd73a3a5a545da78bf5a53a8f06295f2791ab71
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-26 08:39:11 -0400

    gave the email field a onCLick event of handleEmail

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -30,7 +30,7 @@ class GeneralInformation extends React.Component {
       <h1>General Information</h1>
        <TextField handler={this.props.handleName} fieldName="name" className="our-name" labelText="What is your name?"></TextField>
        <label htmlFor="yourEmail">Please Enter Your Email Address:</label>
-       <input className="your-email" id="yourEmail" type='email'></input>
+       <input onClick={this.props.handleEmail} className="your-email" id="yourEmail" type='email'></input>
        <label htmlFor="phoneNumber">Please enter a phone number</label>
         <input className="phone-number" type="tel" placeholder="+1 123-456-789" pattern="[0-9]{3}-[0-9]{3}-{0-9}{4}"></input>
     </div>
