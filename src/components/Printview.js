020ad73e59ba17268c26f9b6c555129611006c7f
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-26 10:55:23 -0400

    gave GeneralInformation, EducationalExperience, PracticalExperience all render methods, and return statements, with headings that mention their sections

--- src/components/Printview.js
+++ src/components/Printview.js
@@ -1,12 +1,18 @@
 import React from 'react';
 class GeneralInformation extends React.Component {
-
+    render() {
+       return <h1>General Information</h1>
+    }
 }
 class EducationalExperience extends React.Component {
-
+    render() {
+        return <h1>Educational Experience</h1>
+    }
 }
 class PracticalExperience extends React.Component {
-
+    render() {
+        return <h1>Practical Experience</h1>
+    }
 }
 class PrintView extends React.Component {
     render() {
