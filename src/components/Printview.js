995632b0de3bb51d75cc326c658ef32f2227cdf0
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-26 10:29:47 -0400

    added three components, GeneralInformation, PracticalExperience and EducationalExperience

--- src/components/Printview.js
+++ src/components/Printview.js
@@ -1,4 +1,13 @@
 import React from 'react';
+class GeneralInformation extends React.Component {
+
+}
+class PracticalExperience extends React.Component {
+
+}
+class EducationalExperience extends React.Component {
+    
+}
 class PrintView extends React.Component {
 
 }
