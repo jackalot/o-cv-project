b6370267ef67ee863f8693a920844ddd1c729c55
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-26 10:34:16 -0400

    The order of Educational and Practical Experience in which they are initialized and called is the same as how the OverView component renders them now

--- src/components/Printview.js
+++ src/components/Printview.js
@@ -2,18 +2,18 @@ import React from 'react';
 class GeneralInformation extends React.Component {
 
 }
-class PracticalExperience extends React.Component {
+class EducationalExperience extends React.Component {
 
 }
-class EducationalExperience extends React.Component {
+class PracticalExperience extends React.Component {
 
 }
 class PrintView extends React.Component {
     render() {
         return <div>
             <GeneralInformation></GeneralInformation>
-            <PracticalExperience></PracticalExperience>
             <EducationalExperience></EducationalExperience>
+            <PracticalExperience></PracticalExperience>
         </div>
     }
 }
