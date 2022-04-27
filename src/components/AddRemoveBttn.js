5c5adcece6eca49e2b8e73a1045627ebd93ddd03
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-27 08:55:55 -0400

    added a comment explaining the AddRemoveButton component

--- src/components/AddRemoveBttn.js
+++ src/components/AddRemoveBttn.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+/* Recieves a field, and allows you to create more of that field or remove them by clicking the two buttons
+this component creates */
 class AddRemoveButton extends React.Component {
     
     render() {
