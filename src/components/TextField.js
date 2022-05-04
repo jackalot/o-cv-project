c6cfd7d2b4451c09ef1ffb9f8aec0b6a580b04fe
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-04 10:01:19 -0400

    converted the TextField class component to a function component

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 
-class TextField extends React.Component {
-    
-    render() {
-        if(this.props.handler)
-        {
-            return <div>
-                <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
-                <input onChange={this.props.handler} className={this.props.className} id={this.props.fieldName} defaultValue={this.props.defaultValue} type='text'></input>
-            </div>
-        }
+function TextField (props) {
+    if(props.handler)
+    {
+        return <div>
+            <label htmlFor={props.fieldName}>{props.labelText}</label>
+            <input onChange={props.handler} className={props.className} id={props.fieldName} defaultValue={props.defaultValue} type='text'></input>
+        </div>
     }
 }
 export default TextField;
