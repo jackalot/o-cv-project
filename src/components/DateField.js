1aea0104f422e501024d95ec78fc31c873d14182
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-04 09:30:07 -0400

    DateField.js now uses a functionalComponent instead of a class component

--- src/components/DateField.js
+++ src/components/DateField.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 
-class DateField extends React.Component {
-    render() {
-        return <div>
-            <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
-            <input onChange={this.props.handler} className={this.props.className} id={this.props.fieldName} defaultValue={this.props.defaultValue} type='date'></input>
-        </div>
-    }
+function DateField (props) {
+    return <div>
+        <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
+        <input onChange={this.props.handler} className={this.props.className} id={this.props.fieldName} defaultValue={this.props.defaultValue} type='date'></input>
+    </div>
 }
 export default DateField;
