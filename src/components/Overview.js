3938fd18544e463ff49f0e5c3ff87679d9d793fb
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-04 10:05:02 -0400

    PracticalExperience in Overview.js is now a functional component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -3,17 +3,16 @@ import NumberField from "./NumberField";
 import DateField from "./DateField";
 import TextField from "./TextField";
 import '../styles/Views.css';
-class PracticalExperience extends React.Component {
-  render () {
-    return <div className="OverView-PracticalExperience">
+function PracticalExperience (props)
+{
+  return <div className="OverView-PracticalExperience">
       <h1>Practical Experience</h1>
-      <TextField handler={this.props.handleCompanyName} fieldName="companyName" className="company-name" labelText="What is the name of your current/last company?"></TextField>
-      <TextField handler={this.props.handlePositionTitle} fieldName="positionTitle" className="position-title" labelText="What is the title of your position at that company?"></TextField>
-      <TextField handler={this.props.handleMainTasks} fieldName="mainTasks" className="main-tasks" labelText="What were your main tasks at that company?"></TextField>
-      <DateField handler={this.props.handleStartDate} fieldName="startDate" className="start-Date" labelText="When did you start working at this position?"></DateField>
-      <DateField handler={this.props.handleEndDate} fieldName="endDate" className="end-Date" labelText="When did you stop working at this position?"></DateField>
+      <TextField handler={props.handleCompanyName} fieldName="companyName" className="company-name" labelText="What is the name of your current/last company?"></TextField>
+      <TextField handler={props.handlePositionTitle} fieldName="positionTitle" className="position-title" labelText="What is the title of your position at that company?"></TextField>
+      <TextField handler={props.handleMainTasks} fieldName="mainTasks" className="main-tasks" labelText="What were your main tasks at that company?"></TextField>
+      <DateField handler={props.handleStartDate} fieldName="startDate" className="start-Date" labelText="When did you start working at this position?"></DateField>
+      <DateField handler={props.handleEndDate} fieldName="endDate" className="end-Date" labelText="When did you stop working at this position?"></DateField>
     </div>
-  }
 }
 class EducationalExperience extends React.Component {
   render () {
