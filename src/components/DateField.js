6e303583a9b55f82a8cd13cb79f8a7ac5b799a8b
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-04 09:32:57 -0400

    removed instances of 'this' in DateField as functions don't require 'this'

--- src/components/DateField.js
+++ src/components/DateField.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 function DateField (props) {
     return <div>
-        <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
-        <input onChange={this.props.handler} className={this.props.className} id={this.props.fieldName} defaultValue={this.props.defaultValue} type='date'></input>
+        <label htmlFor={props.fieldName}>{props.labelText}</label>
+        <input onChange={props.handler} className={props.className} id={props.fieldName} defaultValue={props.defaultValue} type='date'></input>
     </div>
 }
 export default DateField;
