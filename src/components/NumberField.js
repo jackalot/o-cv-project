19b8087d08a729657f7689f798968b7f753ec757
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-04 10:03:08 -0400

    converted NumberField to a functional component

--- src/components/NumberField.js
+++ src/components/NumberField.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 
-class NumberField extends React.Component {
-    render () {
-        return <div>
-            <label htmlFor={this.props.fieldName}>{this.props.labelText}</label>
-        <input className={this.props.className} id={this.props.fieldName} type="number"></input>
-        </div>
-    }
+function NumberField (props)
+{
+    return <div>
+        <label htmlFor={props.fieldName}>{props.labelText}</label>
+        <input className={props.className} id={props.fieldName} type="number"></input>
+    </div>
 }
 export default NumberField;
