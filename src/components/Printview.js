98448321cc00c900bef06c2432c84b9bdfb76cb4
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-04 10:18:52 -0400

    useState and useEffect are now being imported from React in Printview.js

--- src/components/Printview.js
+++ src/components/Printview.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import DateField from "./DateField";
 import TextField from "./TextField";
 import '../styles/Views.css';
